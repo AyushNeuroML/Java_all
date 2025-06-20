f900d2df029cb735b8b51d71a4aeb42e04fa4644
Ayush Chakraborty <theayushchakraborty@gmail.com>
2025-06-20 20:56:39 +0530

    Add files via upload

---
+++ natural.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+
+public class natural {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        int sum = 0;
+        int i = 1;
+        while(i<=n) {
+
+            sum +=i;
+            i++;
+            
+
+        }
+        System.out.println("Natural number is: " + sum);
+        sc.close();
+    }
+    
+}
