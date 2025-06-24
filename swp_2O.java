fafafe378d6b35329271d450c3ae5a6d406c8785
Ayush Chakraborty <theayushchakraborty@gmail.com>
2025-06-24 20:29:51 +0530

    Add files via upload

---
+++ swp_2O.java
@@ -0,0 +1,25 @@
+public class swp_2O {
+
+
+
+        
+
+
+    public static void main(String[] args) {
+
+
+        //swapping two numbers 
+
+        int a = 5;
+        int b = 10;
+
+        int temp = a;
+        a=b;
+        b=temp;
+
+        System.out.println("a = "+a);
+        System.out.println("b = "+b); 
+    }
+
+    
+}
